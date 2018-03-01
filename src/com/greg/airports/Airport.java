f782f4118d60df671d213a4bf0bd6e42d20f9b52
nick <nikoklis@localhost.localdomain>
2018-03-01 03:41:05 +0200

    UI ready functionallity for aircrafts and flights remain

--- src/com/greg/airports/Airport.java
+++ src/com/greg/airports/Airport.java
@@ -2,19 +2,74 @@ package com.greg.airports;
 
 public class Airport {
     private String name;
-    private int[] coordinates = {0,0};
+    private int[] coordinates = {0, 0};
     private int id;
     private int type;
     private int orientation;
     private boolean state;
 
-    public Airport(String name,int x,int y,int id , int type,int orientation,boolean state){
+    public Airport(String name, int x, int y, int id, int type, int orientation, boolean state) {
         coordinates[0] = x;
         coordinates[1] = y;
         this.id = id;
         this.name = name;
         this.type = type;
         this.orientation = orientation;
+        this.state = state;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getState() {
+        if (state == true)
+            return 1;
+        else
+            return 0;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int[] getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(int[] coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(int orientation) {
+        this.orientation = orientation;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
         this.state = state;
     }
 }
--- src/com/greg/mainApp/App.java
+++ src/com/greg/mainApp/App.java
@@ -27,6 +27,10 @@ public class App extends JFrame {
     ArrayList<JPanel> components = new ArrayList<JPanel>();
     ArrayList<Airport> airports = new ArrayList<Airport>();
 
+    //textArea for the right panel
+    //usefull information presented to the user
+    JTextArea information = new JTextArea();
+
 
     public App() {
         app = this;
@@ -56,12 +60,11 @@ public class App extends JFrame {
         //of informative states to the user
         JPanel rightPanel = new JPanel();
         rightPanel.setSize(1200 - 960, 600 - 480);
-        JTextArea jTextArea = new JTextArea();
-        jTextArea.setEditable(false);
-        jTextArea.setPreferredSize(new Dimension(rightPanel.getWidth(), getHeight()));
-        jTextArea.setText("Initializing world...\nLoading world components...\nLoading airports...\n");
 
-        rightPanel.add(jTextArea);
+        information.setEditable(false);
+        information.setPreferredSize(new Dimension(rightPanel.getWidth(), getHeight()));
+
+        rightPanel.add(information);
 
 
         //the mid-left panel used for the main simulation
@@ -72,7 +75,7 @@ public class App extends JFrame {
         createMenuBar(leftPanel);
 
 
-        //splitPane for the 2 middle panels
+        //we need a split pane for the 2 middle panels
         JSplitPane centerPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftPanel, rightPanel);
         centerPane.setDividerSize(0);
         centerPane.setDividerLocation(940);
@@ -86,10 +89,13 @@ public class App extends JFrame {
 
 
         //new pane for all three panels
-        //nested JspliPane to split the screen into 3 parts
+        //nested JsplitPane to split the screen into 3 parts
+        //we provide our first "middle" spliPane and the new Panel of the top
         JSplitPane topPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, centerPane, topPanel);
         topPane.setResizeWeight(0.03);
 
+        //set the top and bottom panels -- bottom via the centerPane we have
+        //that splits the rightPanel and the leftPanel panels
         topPane.setTopComponent(topPanel);
         topPane.setBottomComponent(centerPane);
         this.add(topPane);
@@ -97,12 +103,14 @@ public class App extends JFrame {
         //initialize the Map from mapFile
         initMap(leftPanel);
 
-        String temp = jTextArea.getText();
-        temp += "Initialization complete!";
-        jTextArea.setText(temp);
+
     }
 
     private void initMap(JPanel leftPanel) {
+
+        //string manipulation for world and airportFile
+        //names ++ MAPID --default is -1 if user has not given
+        //something yet
         String worldFileName = "world_";
         String airportFileName = "airports_";
         if (MAPID == -1) {
@@ -118,6 +126,8 @@ public class App extends JFrame {
 
         worldFile = new File(worldFileName);
         airportFile = new File(airportFileName);
+        //check if worldFile and airportFile can be opened to begin
+        //loading information from them
         if (!worldFile.exists() || worldFile.isDirectory() ||
                 !airportFile.exists() || airportFile.isDirectory()) {
             JOptionPane.showMessageDialog(null, "Error finding files with the given MAPID", "Error", JOptionPane.ERROR_MESSAGE);
@@ -127,6 +137,8 @@ public class App extends JFrame {
 
     }
 
+    //actual function to load information from files airportFile
+    //and worldFile
     private void loadInfo(JPanel leftPanel) {
         BufferedReader bufferedReaderWorld = null;
         BufferedReader bufferedReaderAirport = null;
@@ -137,34 +149,54 @@ public class App extends JFrame {
             e.printStackTrace();
         }
 
-        for (JPanel jPanel : components)
-        {
+        //reset the jPanel components to 0
+        //usefull when we reload information
+        //from different file
+        for (JPanel jPanel : components) {
             leftPanel.remove(jPanel);
         }
         components.removeAll(components);
         airports.removeAll(components);
+        ////////
 
+
+        //initialize the world and airports
         initWorld(bufferedReaderWorld, components, leftPanel);
         initAirports(bufferedReaderAirport, airports, components);
+
+        //updates the right panel information for the user
+        information.append("Intialization complete!\n");
     }
 
 
     //helper functions called from the initMap
     private void initWorld(BufferedReader bufferedReaderWorld, ArrayList<JPanel> components, JPanel leftPanel) {
+        //update right panel information
+        information.append("Initializing world\nLoading world components...\n");
         leftPanel.setLayout(new GridLayout(30, 60));
         String line = "";
         String delimeter = ",";
 
         int[] rgbValues;
         try {
+            //start reading the world file to setup the world
             while ((line = bufferedReaderWorld.readLine()) != null) {
                 String[] height = line.split(delimeter);
                 for (int i = 0; i < 60; i++) {
+                    //we create a JPANEL for each rectangle
+                    //in order to draw our world
                     JPanel temp = new JPanel();
+                    //helper function for checking the colour from the input
+                    //we got from the file
                     rgbValues = checkColour(height[i]);
+
+                    //give the rectangles color and size
                     temp.setBackground(new Color(rgbValues[0], rgbValues[1], rgbValues[2]));
                     temp.setSize(16, 16);
+
+                    //update our Arraylist of Jpanel-Rectangles
                     components.add(temp);
+                    //update the leftPanel with the new component
                     leftPanel.add(temp);
                 }
             }
@@ -175,30 +207,48 @@ public class App extends JFrame {
 
     //helperfunctions called from the initMap
     private void initAirports(BufferedReader bufferedReaderAirport, ArrayList<Airport> airports, ArrayList<JPanel> components) {
+        //update the right panel information
+        information.append("Loading airports...\n");
         String line = null;
         String delimeter = ",";
 
         int type;
         int id;
         int orientation;
+        int tempState;
         boolean state;
         int[] coordinates = {0, 0};
+
         try {
+            //start reading the airport file
             while ((line = bufferedReaderAirport.readLine()) != null) {
                 String[] info = line.split(delimeter);
+
                 id = Integer.parseInt(info[0]);
                 coordinates[0] = Integer.parseInt(info[1]);
                 coordinates[1] = Integer.parseInt(info[2]);
                 orientation = Integer.parseInt(info[4]);
                 type = Integer.parseInt(info[5]);
-                state = Boolean.parseBoolean(info[6]);
+
+                //value is an int of 1 or 0 in the file
+                //so we have to check with integer value
+                tempState = Integer.parseInt(info[6]);
+                if (tempState == 1)
+                    state = true;
+                else
+                    state = false;
+
+                //update our Airport ArrrayList with the values we got from the user
                 airports.add(new Airport(info[3], coordinates[0], coordinates[1], id, type, orientation, state));
 
                 //we have a 1D matrix and we get 2 dimensions
                 //so we have to cast the 2 coordinates to our 1D matrix
                 JPanel box = components.get(coordinates[0] * 60 + coordinates[1]);
-                Color tempColor = box.getBackground();
 
+                //we need to set an image on top of our background
+                //so we store the oldvalue and paint an ImageIcon on top
+                //then we reset the background to the old value
+                Color tempColor = box.getBackground();
                 Color oldColor = new Color(tempColor.getRed(), tempColor.getGreen(), tempColor.getBlue());
 
                 box.setLayout(new OverlayLayout(box));
@@ -208,8 +258,6 @@ public class App extends JFrame {
                 image.setBackground(oldColor);
 
                 image.add(new JLabel(imageIcon));
-
-
                 box.add(image);
 
             }
@@ -219,6 +267,8 @@ public class App extends JFrame {
     }
 
 
+    //helper function for checking the
+    //colour to be given to a rectangle
     private int[] checkColour(String colour) {
         int colourValue = Integer.parseInt(colour);
         int[] returnRGB = {0, 0, 0};
@@ -255,8 +305,11 @@ public class App extends JFrame {
         return returnRGB;
     }
 
-    private void createTextInformation(JPanel topPanel) {
 
+    //helper function to initalize the top panel
+    private void createTextInformation(JPanel topPanel) {
+        //create 4 text fields to store the information on
+        //the top panel
         JTextField simulatedTime = createTextLabel("Simulated Time:");
         JTextField totalAircrafts = createTextLabel("Total Aircrafts:");
         JTextField collisions = createTextLabel("Collisions:");
@@ -272,6 +325,8 @@ public class App extends JFrame {
         topPanel.add(landings);
     }
 
+    //helper function for creating the Label
+    //and setting specific values to them
     private JTextField createTextLabel(String s) {
         JTextField jTextField = new JTextField(s);
         jTextField.setEditable(false);
@@ -280,6 +335,8 @@ public class App extends JFrame {
         return jTextField;
     }
 
+
+    
     private void createMenuBar(JPanel leftPanel) {
         JMenuBar menuBar = new JMenuBar();
 
@@ -287,6 +344,7 @@ public class App extends JFrame {
         JMenu simulation = new JMenu("Simulation");
 
 
+        //creating the menu items
         createMenuItem(game, "Start", leftPanel);
         createMenuItem(game, "Stop", leftPanel);
         createMenuItem(game, "Load", leftPanel);
@@ -295,7 +353,7 @@ public class App extends JFrame {
         createMenuItem(simulation, "Airports", leftPanel);
         createMenuItem(simulation, "Aircrafts", leftPanel);
         createMenuItem(simulation, "Flights", leftPanel);
-
+        ////
 
         menuBar.add(game);
         menuBar.add(simulation);
@@ -303,6 +361,7 @@ public class App extends JFrame {
 
         setJMenuBar(menuBar);
     }
+
 
     private void createMenuItem(JMenu jMenu, String infoText, JPanel leftPanel) {
         JMenuItem menuItem = new JMenuItem(infoText);
@@ -315,16 +374,48 @@ public class App extends JFrame {
                             "Please provide with your MapID", null);
                     MAPID = Integer.parseInt(mapID);
 
+                    information.setText("");
+                    information.setText("Reloading...\n");
                     initMap(leftPanel);
                     revalidate();
 //                    repaint();
 
                 }
             });
+        } else if (infoText.equals("Exit")) {
+            menuItem.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    System.exit(0);
+                }
+            });
         } else if (infoText.equals("Airports")) {
             menuItem.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent actionEvent) {
+                    String[] columnNames = {
+                            "ID", "Name", "Category", "State", "Orientation"
+                    };
+
+
+                    JDialog dialog = new JDialog();
+                    Object[][] data = new Object[airports.size()][5];
+                    for (int i = 0; i < airports.size(); i++) {
+                        data[i][0] = airports.get(i).getId();
+                        data[i][1] = airports.get(i).getName();
+                        data[i][2] = airports.get(i).getType();
+                        data[i][3] = airports.get(i).getState();
+                        data[i][4] = airports.get(i).getOrientation();
+                    }
+
+                    JTable jTable = new JTable(data, columnNames);
+                    JScrollPane jScrollPane = new JScrollPane(jTable);
+                    dialog.add(jScrollPane);
+
+                    dialog.setSize(400, 200);
+                    dialog.setTitle("Airports");
+                    dialog.setLocationRelativeTo(null);
+                    dialog.setVisible(true);
                 }
             });
         } else if (infoText.equals("Aircrafts")) {
